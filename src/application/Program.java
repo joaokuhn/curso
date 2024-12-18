1dbf50458d0f76b496ad6e27db39a749ea3ab89f
joao.kuhn <antoniojoao447@gmail.com>
2024-12-18 08:37:05 -0300

    exercicionotas

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,63 +2,30 @@ package application;
 
 import java.util.Scanner;
 
-import entities.Product;
+import entities.Estudante;
 
 public class Program {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
-        String escolha;
+        Estudante estudante = new Estudante();
 
-        Product product = new Product();
-        System.out.println("Enter product data");
-        System.out.println("Name: ");
-        product.name = sc.nextLine();
-        System.out.println("Price: ");
-        product.price = sc.nextDouble();
-        System.out.println("Quantity in stock: ");
-        product.quantity = sc.nextInt();
+        System.out.print("Primeira nota: ");
+        estudante.nota1 = sc.nextDouble();
+        System.out.print("Segunda nota: ");
+        estudante.nota2 = sc.nextDouble();
+        System.out.print("Terceira nota: ");
+        estudante.nota3 = sc.nextDouble();
 
-        System.out.println();
-        System.out.println("Product data: " + product);
+        System.out.println("Nota Final: " + estudante.notaFinal());
 
-        System.out.println("Deseja inserir ou remover produtos?");
-        System.out.println("I - Inserir | R - Remover | E - Encerrar Programa");
-        escolha = sc.next().toUpperCase();
-
-        switch (escolha) {
-            case "I":
-                System.out.println();
-                System.out.println("Enter the number of products to be added in stock: ");
-
-                int quantity = sc.nextInt();
-                product.addProducts(quantity);
-
-                System.out.println();
-                System.out.println("Updated data: " + product);
-                break;
-
-            case "R":
-
-                System.out.println();
-                System.out.println("Enter the number of products to remove: ");
-
-                quantity = sc.nextInt();
-                product.removeProducts(quantity);
-
-                System.out.println();
-                System.out.println("Updated data: " + product);
-                break;
-
-            case "E":
-                System.out.println("Obrigado por usar o app!");
-
-            default:
-                System.out.println("Opção inválida");
-                break;
+        if (estudante.notaFinal() >= 60) {
+            System.out.println("Passou!");
+        } else {
+            System.out.println("Reprovou!");
+            System.out.println("Faltou " + (60 - estudante.notaFinal()) + " pontos");
         }
-
         sc.close();
     }
 }
